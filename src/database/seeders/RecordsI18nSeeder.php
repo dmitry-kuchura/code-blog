5d53c41d456b623c1e0fedf85f5b0f72c7af44cb
Dmytro Kuchura <kuchura.d@gmail.com>
2021-01-20 18:15:46 +0200

    fix seeders

--- src/database/seeders/RecordsI18nSeeder.php
+++ src/database/seeders/RecordsI18nSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -7,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class RecordsI18nSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
